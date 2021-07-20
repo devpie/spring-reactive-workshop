0f90d403b49ea70f374d5fe246b4c248fdbbbe07
Jan Müller <jan.mueller@codecentric.de>
2021-07-20 17:15:16 +0200

    add securityconfig in halloffame

---
+++ HallOfFameDashboard/src/main/java/com/example/dashboard/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.dashboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.authorizeExchange(exchanges ->
+                exchanges
+                        .anyExchange().authenticated()
+        )
+                .httpBasic()
+                .and()
+                .formLogin()
+        .and().oauth2Login();
+        return serverHttpSecurity.build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        return new MapReactiveUserDetailsService(user);
+    }
+
+}
